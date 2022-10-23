5f7b79b33caf8706260e62b7f5cc1b1df2bac788
Axl <AxlBravo.A@gmail.com>
2022-10-23 20:20:59 -0300

    Actualizacion de ubicacion

--- app/src/main/java/com/example/utamaps/MapaGeneral.kt
+++ app/src/main/java/com/example/utamaps/MapaGeneral.kt
@@ -1,11 +1,18 @@
-//Comentario version
+//Actualizacion de ubicacion
 
 package com.example.utamaps
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,11 +23,20 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.example.utamaps.databinding.ActivityMapaGeneralBinding
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 
-class MapaGeneral : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+class MapaGeneral : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapaGeneralBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    private val LOG_TAG = "EnviarUbicacion"
+
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +52,7 @@ class MapaGeneral : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.Con
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        enableLocation()
 
         //Marcador Universidad de Tarapaca
         val universidad = LatLng(-18.490145119500152, -70.29633263195471)
@@ -44,19 +61,98 @@ class MapaGeneral : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.Con
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(universidad, 18F), 3000, null)
     }
 
-    override fun onConnected(p0: Bundle?) {
-        TODO("Not yet implemented")
+    //Funcion para saber si el permiso esta aceptado
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    //Pedir los permisos de Ubicacion
+    private fun requestLocationPermission(){
+        //Si el usuario rechaza los permisos
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION)
+        }
     }
 
-    override fun onConnectionSuspended(p0: Int) {
-        TODO("Not yet implemented")
+    //Funcion para entregar la ubicacion
+    private fun enableLocation(){
+        if (!::mMap.isInitialized) return
+        if (isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = true
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            try {
+                fusedLocationClient.lastLocation.addOnSuccessListener {
+                    if (it != null) {
+                        imprimirUbicacion(it)
+                    } else {
+                        Log.d(LOG_TAG, "No se pudo obtener la ubicación")
+                    }
+                }
+                //////
+                val locationRequest = LocationRequest.create().apply {
+                    interval = 10000
+                    fastestInterval = 5000
+                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                }
+                locationCallback = object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult?) {
+                        locationResult ?: return
+                        Log.d(LOG_TAG, "Se recibió una actualización")
+                        for (location in locationResult.locations) {
+                            imprimirUbicacion(location)
+                        }
+                    }
+                }
+                fusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            } catch (e: SecurityException) {
+                Log.d(LOG_TAG, "Tal vez no solicitaste permiso antes")
+            }
+
+        }
+        else{
+            requestLocationPermission()
+        }
     }
 
-    override fun onConnectionFailed(p0: ConnectionResult) {
-        TODO("Not yet implemented")
+    //Comprueba si el permiso fue aceptado
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                mMap.isMyLocationEnabled = true
+            }
+            else{
+                Toast.makeText(this, "Para activar la localizacion ve a ajuste y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
     }
 
-    override fun onLocationChanged(p0: Location) {
-        TODO("Not yet implemented")
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::mMap.isInitialized) return
+        if (!isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = false
+            Toast.makeText(this, "Para activar la localizacion ve a ajuste y acepta los permisos", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    //-----------------------------------------------------
+    fun imprimirUbicacion(ubicacion: Location) {
+
+        Log.d(LOG_TAG, "Latitud es ${ubicacion.latitude} y la longitud es ${ubicacion.longitude}")
+    }
+
 }
